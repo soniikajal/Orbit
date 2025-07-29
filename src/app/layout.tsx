3594a258d3ee06b280460204f053301e88b7bb6d
Kushagra Kataria <62792734+kushagrakataria@users.noreply.github.com>
2025-07-30 01:55:19 +0530

    vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import '../styles/index.css';
 import AuthProvider from '@/components/auth/AuthProvider';
 import ClientLayout from '@/components/layout/ClientLayout';
+import { Analytics } from '@vercel/analytics/next';
 
 export const viewport = {
   width: 'device-width',
@@ -32,6 +33,7 @@ export default function RootLayout({
             {children}
           </ClientLayout>
         </AuthProvider>
+        <Analytics />
         <script type="module" src="https://static.rocket.new/rocket-web.js?_cfg=https%3A%2F%2Fkajalsap2424back.builtwithrocket.new&_be=https%3A%2F%2Fapplication.rocket.new&_v=0.1.5"></script>
       </body>
     </html>
