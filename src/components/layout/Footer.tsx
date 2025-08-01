14e24f79839179f52534c26b0e19f20843e18431
soniikajal <kajal.soni.ug24@nsut.ac.in>
2025-08-01 08:56:52 +0530

    Phone number removed

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -116,9 +116,7 @@ const Footer: React.FC<FooterProps> = ({ className = '' }) => {
                 <p className="text-base sm:text-lg md:text-xl lg:text-[18px] font-normal leading-5 sm:leading-6 md:leading-7 lg:leading-[23px] text-left text-global-text4 " style={{ fontFamily: 'Inter, sans-serif' }}>
                   Email: nsutorbit@gmail.com
                 </p>
-                <p className="text-base sm:text-lg md:text-xl lg:text-[18px] font-normal leading-5 sm:leading-6 md:leading-7 lg:leading-[23px] text-left text-global-text4 " style={{ fontFamily: 'Inter, sans-serif' }}>
-                  Phone: 7827044075
-                </p>
+                
                 <p className="text-base sm:text-lg md:text-xl lg:text-[18px] font-normal leading-5 sm:leading-6 md:leading-7 lg:leading-[22px] text-left text-global-text4 " style={{ fontFamily: 'Inter, sans-serif' }}>
                   NSUT, Sector 3, Dwarka,<br />New Delhi - 110078
                 </p>
